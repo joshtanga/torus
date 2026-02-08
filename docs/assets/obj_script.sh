76602b847372819aa58983e159dae030e73f3761
Josh Thompson <josh@thedude-2.local>
2026-02-08 11:40:40 -0500

    dir stuff

--- docs/assets/obj_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-obj2gltf -i uv_checkerboard_res_1024_iter_96.obj -o uv_checkerboard_res_1024_iter_96.glb
-obj2gltf -i uv_checkerboard_res_1024_iter_48.obj -o uv_checkerboard_res_1024_iter_48.glb
-obj2gltf -i uv_checkerboard_res_1024_iter_24.obj -o uv_checkerboard_res_1024_iter_24.glb
-obj2gltf -i uv_checkerboard_res_1024_iter_12.obj -o uv_checkerboard_res_1024_iter_12.glb
